98f3b605269c1a03dae631f84a40ae66901663e8
Alex Liu <alex_liu@epam.com>
2019-01-18 11:20:48 +0800

    add some empty pages

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # vue-netease-music
+用vue写的网易云音乐网页版
+Api来自NeteaseCloudMusicApi
+部署在[heroku](https://netease-music-api.herokuapp.com)上
 
 ## Project setup
 ```
---
+++ src/components/WebHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <router-link to="/">发现音乐</router-link>
+    <router-link to="/">我的音乐</router-link>
+    <router-link to="/">朋友</router-link>
+    <router-link to="/">商城</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebHeader',
+  props: {
+    
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
